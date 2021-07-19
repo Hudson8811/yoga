06534aa8d4e4ef0f876eae8ca76437fb1c081bcc
burdms <burdms@gmail.com>
2021-07-19 16:29:19 +0200

    create: modal responsive

--- src/js/partials/weekly.js
+++ src/js/partials/weekly.js
@@ -1,4 +1,22 @@
 if (document.querySelector(".weekly")) {
+  // Modal
+  const events = document.querySelector(".weekly-posts"),
+    modal = document.querySelector(".modal");
+
+  document.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target.closest(".weekly-event__comments") && !modal.classList.contains("active")) {
+      modal.classList.add("active");
+      document.body.classList.add("js-body-noscroll");
+    }
+
+    if (target.closest(".modal__close") && modal.classList.contains("active")) {
+      modal.classList.remove("active");
+      document.body.classList.remove("js-body-noscroll");
+    }
+  });
+
   const stickySidebar = () => {
     const stickyElement = document.querySelector(".part-wsidebar__sidebar--weekly");
 
