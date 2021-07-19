4fb4dda15f081ffa74a2db8e387e3dd5cf66d7de
burdms <burdms@gmail.com>
2021-07-19 16:38:49 +0200

    create: likes on event cards .js

--- build/js/main.js
+++ build/js/main.js
@@ -3583,21 +3583,25 @@ $(document).ready(function () {
 
 // Weekly Events page
 if (document.querySelector(".weekly")) {
-  // Modal
-  const events = document.querySelector(".weekly-posts"),
-    modal = document.querySelector(".modal");
+  const modal = document.querySelector(".modal");
 
   document.addEventListener("click", (event) => {
     const target = event.target;
 
+    // Open modal on 'comments' click
     if (target.closest(".weekly-event__comments") && !modal.classList.contains("active")) {
       modal.classList.add("active");
       document.body.classList.add("js-body-noscroll");
     }
 
+    // Close modal on 'close' click
     if (target.closest(".modal__close") && modal.classList.contains("active")) {
       modal.classList.remove("active");
       document.body.classList.remove("js-body-noscroll");
+    }
+
+    if (target.closest(".weekly-event__likes")) {
+      target.closest(".weekly-event__likes").classList.toggle("liked");
     }
   });
 
