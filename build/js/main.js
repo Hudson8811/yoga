2f2b27e15ebe10018ae569adc53c6c2a051298de
PipZip <37911683+PipZip@users.noreply.github.com>
2021-07-21 16:06:43 +0600

    fix

--- build/js/main.js
+++ build/js/main.js
@@ -2157,7 +2157,7 @@ $(document).ready(function () {
 	$('.js-kspc-twpah-unwrap').click(function () {
 		$(this).addClass('kspc-twpah-unwrap--hide').closest('.kspc-twpah').addClass('kspc-twpah--open');
 	});
-	kspc-simple-text--has-small-title
+
 
 	$('.js-simple-text-unwrap').click(function () {
 		$(this).addClass('kspc-twpah-unwrap--hide').closest('.kspc-simple-text').addClass('kspc-simple-text--open');
@@ -3410,81 +3410,158 @@ $(document).ready(function () {
 		function initCalcCE(){
 			let itemsCE = $('.ce-i');
 			itemsCE.each(function (){
-				let ce = $(this);
-				let ceImage = ce.find('.ce-i__img');
-				let ceImageHeight = ceImage.outerHeight();
-				let ceHeight = ce.find('.ce-i__left').innerHeight();
-				if (ceHeight > ceImageHeight){
-					let ceText = ce.find('.ce-i__text');
-					let ceMoreHtml = '<div class="ce-i__more"><span></span><a href="javascript:;">Развернуть</a></div>';
-					if (ceText.length){
-						let ceTextHeight = ceText.outerHeight();
-						let diffHeight = ceHeight - ceImageHeight;
-						let maxHeight = ceTextHeight - diffHeight;
-						let lineHeight = parseFloat(ce.find('p, li').css('line-height'));
-						let calcHeight = 0;
-						let elemsCount = 0;
-						ce.find('.ce-i__text > *').each(function (){
-							let elem = $(this);
-							let elemHeight = elem.outerHeight();
-							if (elemsCount > 0){
-								let marginTop = parseFloat(elem.css('margin-top'));
-								if (marginTop + calcHeight + lineHeight >= maxHeight){
-									return false;
-								} else {
-									calcHeight += marginTop;
+				if ($(window).width() > 749){
+					let ce = $(this);
+					let ceImage = ce.find('.ce-i__img');
+					let ceImageHeight = ceImage.outerHeight();
+					let ceHeight = ce.find('.ce-i__left').innerHeight();
+					if (ceHeight > ceImageHeight){
+						let ceText = ce.find('.ce-i__text');
+						let ceMoreHtml = '<div class="ce-i__more"><span></span><a href="javascript:;">Развернуть</a></div>';
+						if (ceText.length){
+							let ceTextHeight = ceText.outerHeight();
+							let diffHeight = ceHeight - ceImageHeight;
+							let maxHeight = ceTextHeight - diffHeight;
+							let lineHeight = parseFloat(ce.find('p, li').css('line-height'));
+							let calcHeight = 0;
+							let elemsCount = 0;
+							ce.find('.ce-i__text > *').each(function (){
+								let elem = $(this);
+								let elemHeight = elem.outerHeight();
+								if (elemsCount > 0){
+									let marginTop = parseFloat(elem.css('margin-top'));
+									if (marginTop + calcHeight + lineHeight >= maxHeight){
+										return false;
+									} else {
+										calcHeight += marginTop;
+									}
 								}
-							}
-							if (maxHeight - calcHeight >= lineHeight){
-								switch (elem.prop("tagName")){
-									case 'UL':
-										if (calcHeight + elemHeight > maxHeight){
-											let lis = elem.find('li');
-											let lisCount = 0;
-											lis.each(function (){
-												let li = $(this);
-												let liHeight = li.outerHeight();
-												if (lisCount > 0){
-													let liMarginTop = parseFloat(li.css('margin-top'));
-													if (calcHeight + liMarginTop > maxHeight || maxHeight - calcHeight - liMarginTop < lineHeight){
+								if (maxHeight - calcHeight >= lineHeight){
+									switch (elem.prop("tagName")){
+										case 'UL':
+											if (calcHeight + elemHeight > maxHeight){
+												let lis = elem.find('li');
+												let lisCount = 0;
+												lis.each(function (){
+													let li = $(this);
+													let liHeight = li.outerHeight();
+													if (lisCount > 0){
+														let liMarginTop = parseFloat(li.css('margin-top'));
+														if (calcHeight + liMarginTop > maxHeight || maxHeight - calcHeight - liMarginTop < lineHeight){
+															return false;
+														} else {
+															calcHeight += liMarginTop;
+														}
+													}
+													if (calcHeight + liHeight > maxHeight){
+														let countRows = ((maxHeight-calcHeight)/lineHeight|0);
+														calcHeight += lineHeight*countRows;
 														return false;
 													} else {
-														calcHeight += liMarginTop;
+														calcHeight += liHeight;
 													}
-												}
-												if (calcHeight + liHeight > maxHeight){
-													let countRows = ((maxHeight-calcHeight)/lineHeight|0);
-													calcHeight += lineHeight*countRows;
-													return false;
-												} else {
-													calcHeight += liHeight;
-												}
-												lisCount++;
-											});
-											return false;
-										} else {
-											calcHeight += elemHeight;
-										}
-										break;
-									default:
-										if (calcHeight + elemHeight > maxHeight){
-											let countRows = ((maxHeight-calcHeight)/lineHeight|0);
-											calcHeight += lineHeight*countRows;
-											return false;
-										} else {
-											calcHeight += elemHeight;
-										}
+													lisCount++;
+												});
+												return false;
+											} else {
+												calcHeight += elemHeight;
+											}
+											break;
+										default:
+											if (calcHeight + elemHeight > maxHeight){
+												let countRows = ((maxHeight-calcHeight)/lineHeight|0);
+												calcHeight += lineHeight*countRows;
+												return false;
+											} else {
+												calcHeight += elemHeight;
+											}
+									}
+
+									elemsCount++;
+								} else{
+									return false;
 								}
+							});
 
-								elemsCount++;
-							} else{
-								return false;
-							}
-						});
+							ceText.css({
+								'max-height': calcHeight+'px'
+							}).addClass('maxed').append(ceMoreHtml);
+						}
+					}
+				} else {
+					let ce = $(this);
+					let ceText = ce.find('.ce-i__text');
+					if (ceText.length){
+						let ceMoreHtml = '<div class="ce-i__more"><span></span><a href="javascript:;">... Развернуть</a></div>';
+						let ceTextHeight = ceText.outerHeight();
+						let lineHeight = parseFloat(ce.find('p, li').css('line-height'));
+						let maxHeight = lineHeight*3;
+						if (ce.hasClass('ce-i--big')) maxHeight = lineHeight*4;
+						if (ceTextHeight > maxHeight){
+							let calcHeight = 0;
+							let elemsCount = 0;
+							ce.find('.ce-i__text > *').each(function (){
+								let elem = $(this);
+								let elemHeight = elem.outerHeight();
+								if (elemsCount > 0){
+									let marginTop = parseFloat(elem.css('margin-top'));
+									if (marginTop + calcHeight + lineHeight >= maxHeight){
+										return false;
+									} else {
+										calcHeight += marginTop;
+									}
+								}
+								if (maxHeight - calcHeight >= lineHeight){
+									switch (elem.prop("tagName")){
+										case 'UL':
+											if (calcHeight + elemHeight > maxHeight){
+												let lis = elem.find('li');
+												let lisCount = 0;
+												lis.each(function (){
+													let li = $(this);
+													let liHeight = li.outerHeight();
+													if (lisCount > 0){
+														let liMarginTop = parseFloat(li.css('margin-top'));
+														if (calcHeight + liMarginTop > maxHeight || maxHeight - calcHeight - liMarginTop < lineHeight){
+															return false;
+														} else {
+															calcHeight += liMarginTop;
+														}
+													}
+													if (calcHeight + liHeight > maxHeight){
+														let countRows = ((maxHeight-calcHeight)/lineHeight|0);
+														calcHeight += lineHeight*countRows;
+														return false;
+													} else {
+														calcHeight += liHeight;
+													}
+													lisCount++;
+												});
+												return false;
+											} else {
+												calcHeight += elemHeight;
+											}
+											break;
+										default:
+											if (calcHeight + elemHeight > maxHeight){
+												let countRows = ((maxHeight-calcHeight)/lineHeight|0);
+												calcHeight += lineHeight*countRows;
+												return false;
+											} else {
+												calcHeight += elemHeight;
+											}
+									}
 
-						ceText.css({
-							'max-height': calcHeight+'px'
-						}).addClass('maxed').append(ceMoreHtml);
+									elemsCount++;
+								} else{
+									return false;
+								}
+							});
+
+							ceText.css({
+								'max-height': calcHeight+'px'
+							}).addClass('maxed').append(ceMoreHtml);
+						}
 					}
 				}
 			});
