0509e844569fe3fb7a11d42cffda3812a5db3898
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-20 17:22:39 +0300

    123

--- src/js/partials/weekly.js
+++ src/js/partials/weekly.js
@@ -5,4 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 		additionalMarginTop: 30,
 		additionalMarginBottom: -30,
 	});
+
+	$('body').on('click','.js-weekly-event__unwrap-tablet', function(){
+		$(this).addClass('weekly-event__unwrap-tablet--hidden').siblings('.js-text-wrapped').find('.js-text-unwrapper').click();
+	});
+
+
+
 });
